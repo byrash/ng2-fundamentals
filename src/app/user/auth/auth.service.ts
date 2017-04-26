77285e88f8f0e7d4e7ed2726c1a560e8b01fcb3a
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2017-04-26 17:10:09 +1000

    Login Module from MEAN

--- src/app/user/auth/auth.service.ts
+++ src/app/user/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
 import {IUser} from "./user.model";
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 /**
  * Created by Shivaji on 16/3/17.
  */
@@ -7,21 +9,36 @@ import {IUser} from "./user.model";
 export class AuthService {
     currentUser: IUser
 
+    constructor(private http: Http) {
+    }
+
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'Shiv @'+userName,
-            lastName: 'Byra'
-        }
+        // this.currentUser = {
+        //     id: 1,
+        //     userName: userName,
+        //     firstName: 'Shiv @'+userName,
+        //     lastName: 'Byra'
+        // }
+        let loginInfo = {uname: userName, pwd: password};
+        return this.http.post('http://localhost:8080/login', JSON.stringify(loginInfo))
+        //Do does not subscribe and will wait work on response once when some one subscribes to it
+            .do((resp: Response) => {
+                if (resp) {// If not an error
+                    this.currentUser = <IUser>resp.json();
+                }
+            }).catch((resp: Response) => {
+                //It will be an error on unautheticated case and we wanted to send the caller that
+                // its not an error and want to send false
+                return Observable.of(false);
+            });
     }
 
     isAuthenticated() {
         return !!this.currentUser;
     }
 
-    updateCurrentUser(firstName:string,lastName:string){
-        this.currentUser.firstName= firstName;
+    updateCurrentUser(firstName: string, lastName: string) {
+        this.currentUser.firstName = firstName;
         this.currentUser.lastName = lastName;
     }
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,20 +6,31 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
  */
 @Component({
     templateUrl: 'app/user/login/login.component.html',
-    styles:[`
-        em {float: right; color: #E05C65; padding-left: 10px}
-        `]
+    styles: [`
+        em {
+            float: right;
+            color: #E05C65;
+            padding-left: 10px
+        }
+    `]
 })
-export class LoginComponent implements OnInit{
-    redirectUrl : String = 'events';
+export class LoginComponent implements OnInit {
+    redirectUrl: String = 'events';
+    loginInvalid = false;
 
     constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
 
     }
 
     login(formValues) {
-        this.authService.loginUser(formValues.userName, formValues.password);
-        this.router.navigate([this.redirectUrl]);
+        this.authService.loginUser(formValues.userName, formValues.password).subscribe(resp => {
+            if (!resp) {
+                this.loginInvalid = true;
+            } else {
+                this.router.navigate([this.redirectUrl]);
+            }
+        });
+
     }
 
     cancel() {
@@ -29,9 +40,9 @@ export class LoginComponent implements OnInit{
 
     ngOnInit(): void {
         this.route.params.forEach((param: Params) => {
-            if(param['eventId']){
-                this.redirectUrl = 'event/'+param['eventId'];
-            }else{
+            if (param['eventId']) {
+                this.redirectUrl = 'event/' + param['eventId'];
+            } else {
                 this.redirectUrl = 'events';
             }
         });
