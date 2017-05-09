e44ee95d3478d3fff7b075a8645fffbcbde7fd58
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2017-05-09 16:56:12 +1000

    Karma config

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var events_app_module_1 = require("./app/events-app.module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(events_app_module_1.EventsAppModule);
+//# sourceMappingURL=main.js.map
---
+++ src/test/test.spec.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Shivaji on 28/4/17.
+ */
+describe('First Test', () => {
+    it('Should be true', () => {
+        expect(true).toBe(true);
+    });
+});
